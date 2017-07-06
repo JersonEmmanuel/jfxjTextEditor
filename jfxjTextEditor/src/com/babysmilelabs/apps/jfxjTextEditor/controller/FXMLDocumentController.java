1411e8fc4f5bb0e7bc9206c0c85b62a101bcda69
Jerson Emmanuel Martinez Zaragoza <jerson_uni@hotmail.com>
2017-07-06 11:45:26 -0500

    Upload the files
    Of the jfxjTextEditor javaFX project.

---
+++ jfxjTextEditor/src/com/babysmilelabs/apps/jfxjTextEditor/controller/FXMLDocumentController.java
@@ -0,0 +1,30 @@
+package com.babysmilelabs.apps.jfxjTextEditor.controller;
+
+import java.net.URL;
+import java.util.ResourceBundle;
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
+import javafx.scene.control.Label;
+
+/**
+ * @author Jerson Emmanuel Martinez Zaragoza
+ * @since 06 de julio de 2017
+ */
+public class FXMLDocumentController implements Initializable{
+    
+    @FXML
+    private Label label;
+    
+    @FXML
+    private void handleButtonAction(ActionEvent event){
+        System.out.println("You clicked me!");
+        label.setText("Hello World!");
+    }
+    
+    @Override
+    public void initialize(URL url, ResourceBundle rb){
+        // TODO
+    }    
+    
+}
